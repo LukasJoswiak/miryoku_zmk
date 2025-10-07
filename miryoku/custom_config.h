935b74d2ad9ab8b104fee54164b2c36e1c992b58
Lukas Joswiak <lukas@lukasjoswiak.com>
2025-10-06 22:56:49 -0700

    Swap to CAGS home row mods

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,9 +2,3 @@
 // https://github.com/manna-harbour/miryoku
 
 #define MIRYOKU_ALPHAS_DVORAK
-
-// Swap CTRL and CMD
-#define MIRYOKU_LMOD_LCTL LGUI
-#define MIRYOKU_LMOD_LGUI LCTL
-#define MIRYOKU_RMOD_RCTL RGUI
-#define MIRYOKU_RMOD_RGUI RCTL
